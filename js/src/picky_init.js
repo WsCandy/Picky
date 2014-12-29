bac4691eee7a38951ee8f57a957109636add28e8
Samuel <s-bridge@live.co.uk>
2014-12-29 15:14:10 +0000

    [Styles] - Style tweaks

--- js/src/picky_init.js
+++ js/src/picky_init.js
@@ -2,11 +2,11 @@ $(document).on('ready', function() {
 
 	$('.picky').picky({
 
-		disable: ['2015-01-10'],
+		disable: ['2015-01-10', '2015-01-15'],
 		disablePast: true,
 		disableDays: [],
 		labels: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-		advance: 0
+		advance: 3
 
 	});
 
