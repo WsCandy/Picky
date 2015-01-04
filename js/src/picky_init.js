740ca0c7a4d5fb7ed88d4fed387ba8e48c7ca98e
Samuel <s-bridge@live.co.uk>
2015-01-04 00:08:15 +0000

    [JS] - Disabled dates testing

--- js/src/picky_init.js
+++ js/src/picky_init.js
@@ -2,7 +2,7 @@ $(document).on('ready', function() {
 
 	$('.picky').picky({
 
-		disable: ['2015-01-08', ['2015-01-16', '2015-01-18'], '2015-01-14'],
+		disable: ['2015-01-08', ['2015-01-16', '2015-01-18'], '2015-01-14', '2015-08-02', ['2015-08-06', '2015-08-10']],
 		disablePast: true,
 		disableDays: [],
 		labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
